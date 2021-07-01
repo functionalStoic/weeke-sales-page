afb91f343e30669118a359b8be262183f29cc5d9
JasonSooter <7215306+functionalStoic@users.noreply.github.com>
2021-07-01 14:54:31 -0600

    feat(content): add recaptcha validation - https://docs.netlify.com/forms/spam-filters/#netlify-provided-recaptcha-2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,7 +3,12 @@ import React from 'react';
 export default function ContactForm() {
   return (
     <div style={{ margin: '0 auto', maxWidth: '700px' }}>
-      <form name="contact" method="POST" data-netlify="true">
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        data-netlify-recaptcha="true"
+      >
         <Field>
           Your Name: <input type="text" name="name" />
         </Field>
