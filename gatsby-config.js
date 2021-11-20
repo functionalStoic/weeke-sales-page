8e31a58e153d70d9f6bada0ec92f08dca87959bd
JasonSooter <7215306+functionalStoic@users.noreply.github.com>
2021-11-20 16:47:49 -0700

    chore(deps): remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Makor Q-One - For Sale',
+    title: 'Weeke CNC - For Sale',
     author: 'Jason Sooter',
-    description: 'Makor Q-One for sale. Excellent shape with light use'
+    description: 'Weeke CNC for sale. Excellent shape with light use'
   },
   pathPrefix: '/',
   plugins: [
@@ -37,16 +37,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-119664067-1',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/']
-      }
-    }
+    `gatsby-plugin-sass`
   ]
 };
