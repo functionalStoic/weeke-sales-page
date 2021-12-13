c99f321328afadf8cb1f07bb93954b1a03acb93a
JasonSooter <7215306+functionalStoic@users.noreply.github.com>
2021-12-12 18:45:09 -0700

    feat(content): replace text, details, update minor styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,38 +4,65 @@ export default function Header() {
   return (
     <section id="header">
       <div className="inner">
-        <span className="icon major fa-home" />
         <h1>
           Hi, I'm <strong>Jason.</strong> I'm selling our
           <br />
-          Makor Q-One Reciprocating Spray Machine.
+          Weeke 5x10 CNC Router.
         </h1>
-        <h3>
-          Lightly used. Clean and in excellent shape. <br />
-          Used to spray only 1 product, a pre-cat lacquer clear finish.
-          <br />
-          <b>$50,000+</b> savings compared with buying a new one
-        </h3>
+        <ul>
+          <li>Weeke Optimat BHP008 Vantech 510</li>
+          <li>Clean and in excellent shape. Spindle Rebuilt in 2021</li>
+          <li><b>$50,000+</b> savings compared with buying a new one</li>
+        </ul>
         <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/5pyCRGgRfkc?rel=0"
+          width="100%"
+          style={{ maxWidth: 750 }}
+          height="422"
+          src="https://www.youtube.com/embed/Bws8X6uZmtU"
+          title="Weeke CNC Demo Video"
           frameBorder="0"
-          allow="autoplay; encrypted-media"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-          title="demo video"
         />
         <ul className="actions">
           <li>
             <a
-              aria-label="discover"
-              href="#one"
+              aria-label="photo-gallery"
+              href="#four"
               className="button scrolly"
-              children="Discover"
+              children="View Photo Gallery"
+              onClick={() => window.analytics ? window.analytics.track("Button Clicked", { section: "Photo Gallery" }) : null}
+            />
+          </li>
+          <li>
+            <a
+              aria-label="technical-specs"
+              href="#five"
+              className="button scrolly"
+              children="View Technical Specs"
+              onClick={() => window.analytics ? window.analytics.track("Button Clicked", { section: "Technical Specs" }) : null}
+            />
+          </li>
+          <li>
+            <a
+              aria-label="key-features"
+              href="#six"
+              className="button scrolly"
+              children="Key Features"
+              onClick={() => window.analytics ? window.analytics.track("Button Clicked", { section: "Key Features" }) : null}
+            />
+          </li>
+          <li>
+            <a
+              aria-label="contact"
+              href="#six"
+              className="button scrolly"
+              children="Contact"
+              onClick={() => window.analytics ? window.analytics.track("Button Clicked", { section: "Contact" }) : null}
             />
           </li>
         </ul>
       </div>
-    </section>
+    </section >
   );
 }
--- src/components/Section5.js
+++ src/components/Section5.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
 export default function SectionFive() {
-  return <div></div>;
+  return (
+    <section id="three" className="main style1 special">
+      <div className="grid-wrapper">
+        <div className="col-12">
+          <header className="major">
+            <h2>Key Features</h2>
+          </header>
+          <p>Here is more info to help you make your decision</p>
+        </div>
+        <div className="col-4">
+          <h3>Key Feature 1</h3>
+          <p>About Key Feature 1</p>
+        </div>
+        <div className="col-4">
+          <h3>Key Feature 2</h3>
+          <p>About Key Feature 2</p>
+        </div>
+        <div className="col-4">
+          <h3>Key Feature 3</h3>
+          <p>About Key Feature 3</p>
+        </div>
+
+      </div>
+    </section>
+  );
 }
